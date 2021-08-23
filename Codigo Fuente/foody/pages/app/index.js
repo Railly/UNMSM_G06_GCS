9b6c84a7c5a9044f476c634791c6f61ec9ededfc
Carlos <carlos-100-800@hotmail.com>
2021-08-22 21:11:23 -0500

    FIX: Fix styles of browse page

--- Codigo Fuente/foody/pages/app/index.js
+++ Codigo Fuente/foody/pages/app/index.js
@@ -1,11 +1,10 @@
 import AuthButton from 'components/AuthButton'
 import { signOut } from 'firebase/client'
 import useUser from 'hooks/useUser'
-import { useRouter } from 'next/router' 
 
 export default function App () {
   const user = useUser()
-  const router = useRouter()
+  console.log(user)
 
   const handleClick = () => {
     signOut().then(() => {})
@@ -14,17 +13,17 @@ export default function App () {
   return (
     <>
            {!user
-        ? (
+             ? (
         <h1>Cargando</h1>
-          )
-        : (
+               )
+             : (
         <>
           <h1>App</h1>
           <AuthButton onClick={handleClick} type="primary">
             Cerrar Sesion
           </AuthButton>
         </>
-          )}
+               )}
     </>
   )
 }
